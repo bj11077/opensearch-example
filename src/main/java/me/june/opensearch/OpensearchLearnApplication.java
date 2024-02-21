d59e29289ca5f6589dd48866b7e619096fbe3141
bjune-park <54969248+bj11077@users.noreply.github.com>
2024-02-21 22:28:59 +0900

    init

---
+++ src/main/java/me/june/opensearch/OpensearchLearnApplication.java
@@ -0,0 +1,13 @@
+package me.june.opensearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpensearchLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpensearchLearnApplication.class, args);
+    }
+
+}
